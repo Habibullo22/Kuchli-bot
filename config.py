e7bcd3c8cfec42f424934cd12e9ad5ac51484fe2
Habibullo22 <habibulloabidjonov555@gmail.com>
2026-08-17 17:56:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8611335484:AAHoH8mg1OO9V7PDB3wUlZ6wU3HxsQx7avY")
-API_ID = int(os.getenv("33997393"))
-API_HASH = os.getenv("fdc3748fef684caff6e5d72fa667c829")
-ADMIN_ID = int(os.getenv("5815294733"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
